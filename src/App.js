0deeebcd0b015b068ab0a289a2a8e202dd14d394
mule <belmeetmule@gmail.com>
2022-09-17 20:54:06 +0300

    Clear app.js

--- src/App.js
+++ src/App.js
@@ -2,30 +2,11 @@ import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      calculatorData: {
-        total: null,
-        next: null,
-        operation: null,
-      },
-    };
-    this.updateState = this.updateState.bind(this);
-  }
-
-  updateState(obj) {
-    this.setState({
-      calculatorData: obj,
-    });
-  }
-
+class App extends React.PureComponent {
   render() {
-    const { calculatorData } = this.state;
     return (
       <div className="App">
-        <Calculator calculatorData={calculatorData} updateState={this.updateState} />
+        <Calculator />
       </div>
     );
   }
