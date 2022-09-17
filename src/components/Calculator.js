3f48ab760c4acdb5b20c291b7e9186996666ef07
mule <belmeetmule@gmail.com>
2022-09-17 03:28:09 +0300

    add state event listner to calculator keys

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,37 +1,229 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import calculate from '../logic/calculate';
 
-class Calculator extends React.PureComponent {
+class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.Compute = this.Compute.bind(this);
+    this.updateScreen = this.updateScreen.bind(this);
   }
 
+  Compute = (obj, button) => {
+    const { updateState } = this.props;
+    const newObj = calculate(obj, button);
+    updateState(newObj);
+  };
+
+  updateScreen = () => {
+    const { digitObj } = this.props;
+
+    const { total, next, operation } = digitObj;
+
+    if (total === null && next === null) {
+      return 0;
+    }
+    if (next !== null && total === null) {
+      return next;
+    }
+    if (total !== null && next !== null) {
+      if (operation !== null && operation !== undefined) {
+        return `${total} ${operation} ${next}`;
+      }
+      return total;
+    }
+    return total;
+  };
+
   render() {
+    const { digitObj } = this.props;
     return (
       <div className="container">
-        <div className="keys screen"> 0 </div>
-        <button type="button" className="keys">AC</button>
-        <button type="button" className="keys">+/-</button>
-        <button type="button" className="keys">%</button>
-        <button type="button" className="keys operators">/</button>
-        <button type="button" className="keys ">7</button>
-        <button type="button" className="keys">8</button>
-        <button type="button" className="keys">9</button>
-        <button type="button" className="keys operators">x</button>
-        <button type="button" className="keys">4</button>
-        <button type="button" className="keys">5</button>
-        <button type="button" className="keys">6</button>
-        <button type="button" className="keys operators">-</button>
-        <button type="button" className="keys">1</button>
-        <button type="button" className="keys">2</button>
-        <button type="button" className="keys">3</button>
-        <button type="button" className="keys operators">+</button>
-        <button type="button" className="keys zero">0</button>
-        <button type="button" className="keys">.</button>
-        <button type="button" className="keys operators">=</button>
+        <div className="keys screen">
+          {this.updateScreen()}
+        </div>
+        <button
+          type="button"
+          className="keys"
+          onClick={() => {
+            this.Compute(digitObj, 'AC');
+          }}
+        >
+          AC
+        </button>
+        <button
+          type="button"
+          className="keys"
+          onClick={() => {
+            this.Compute(digitObj, '+/-');
+          }}
+        >
+          +/-
+        </button>
+        <button
+          type="button"
+          className="keys"
+          onClick={() => {
+            this.Compute(digitObj, '%');
+          }}
+        >
+          %
+        </button>
+        <button
+          type="button"
+          className="keys operators"
+          onClick={() => {
+            this.Compute(digitObj, '÷');
+          }}
+        >
+          ÷
+        </button>
+        <button
+          type="button"
+          className="keys "
+          value="7"
+          onClick={() => {
+            this.Compute(digitObj, '7');
+          }}
+        >
+          7
+        </button>
+        <button
+          type="button"
+          className="keys"
+          value="8"
+          onClick={() => {
+            this.Compute(digitObj, '8');
+          }}
+        >
+          8
+        </button>
+        <button
+          type="button"
+          className="keys"
+          onClick={() => {
+            this.Compute(digitObj, '9');
+          }}
+        >
+          9
+        </button>
+        <button
+          type="button"
+          className="keys operators"
+          onClick={() => {
+            this.Compute(digitObj, 'x');
+          }}
+        >
+          x
+        </button>
+        <button
+          type="button"
+          className="keys"
+          onClick={() => {
+            this.Compute(digitObj, '4');
+          }}
+        >
+          4
+        </button>
+        <button
+          type="button"
+          className="keys"
+          onClick={() => {
+            this.Compute(digitObj, '5');
+          }}
+        >
+          5
+        </button>
+        <button
+          type="button"
+          className="keys"
+          onClick={() => {
+            this.Compute(digitObj, '6');
+          }}
+        >
+          6
+        </button>
+        <button
+          type="button"
+          className="keys operators"
+          onClick={() => {
+            this.Compute(digitObj, '-');
+          }}
+        >
+          -
+        </button>
+        <button
+          type="button"
+          className="keys"
+          onClick={() => {
+            this.Compute(digitObj, '1');
+          }}
+        >
+          1
+        </button>
+        <button
+          type="button"
+          className="keys"
+          onClick={() => {
+            this.Compute(digitObj, '2');
+          }}
+        >
+          2
+        </button>
+        <button
+          type="button"
+          className="keys"
+          onClick={() => {
+            this.Compute(digitObj, '3');
+          }}
+        >
+          3
+        </button>
+        <button
+          type="button"
+          className="keys operators"
+          onClick={() => {
+            this.Compute(digitObj, '+');
+          }}
+        >
+          +
+        </button>
+        <button
+          type="button"
+          className="keys zero"
+          onClick={() => {
+            this.Compute(digitObj, '0');
+          }}
+        >
+          0
+        </button>
+        <button
+          type="button"
+          className="keys"
+          onClick={() => {
+            this.Compute(digitObj, '.');
+          }}
+        >
+          .
+        </button>
+        <button
+          type="button"
+          className="keys operators"
+          onClick={() => {
+            this.Compute(digitObj, '=');
+          }}
+        >
+          =
+        </button>
       </div>
     );
   }
 }
+
+Calculator.propTypes = {
+  updateState: PropTypes.func.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  digitObj: PropTypes.object.isRequired,
+};
 
 export default Calculator;
