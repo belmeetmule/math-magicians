03320590d0c47d0cce47098b1ff8e269ad4d3e2f
mule <belmeetmule@gmail.com>
2022-09-22 17:26:50 +0300

    add route to different components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Quote from './components/Quote';
+// import NotMatch from "../pages/NotMatch"
 
 class App extends React.PureComponent {
   render() {
     return (
-      <div className="App">
-        <Calculator />
-      </div>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Calculator" element={<Calculator />} />
+          <Route path="/Quote" element={<Quote />} />
+        </Routes>
+      </>
     );
   }
 }
